3bcc955eeb278d2782f21582280e0f475da3ac3e
Dat Nguyen <thanhdat.nguyen>
2020-08-06 23:02:57 +0800

    Update unsafe lifecycle methods

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -213,7 +213,7 @@ export default class AgendaView extends Component {
     }
   }
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this._isMounted = true;
     this.loadReservations(this.props);
   }
@@ -222,7 +222,7 @@ export default class AgendaView extends Component {
     this._isMounted = false;
   }
 
-  componentWillReceiveProps(props) {
+  UNSAFE_componentWillReceiveProps(props) {
     if (props.items) {
       this.setState({
         firstResevationLoad: false
